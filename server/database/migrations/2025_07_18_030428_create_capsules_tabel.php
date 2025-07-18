b6294ca0455ae34978a1391ba46ea3a024134f52
ALi Shhoury <alishhoury78@gmail.com>
2025-07-18 09:13:04 +0300

    migrattion tables for capsules

--- server/database/migrations/2025_07_18_030428_create_capsules_tabel.php
+++ server/database/migrations/2025_07_18_030428_create_capsules_tabel.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\User;
+
 return new class extends Migration
 {
     /**
@@ -11,9 +13,42 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('capsules_tabel', function (Blueprint $table) {
+        Schema::create('capsules', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->date('reveal_at');
+            $table->boolean('is_revealed')->default(false);
             $table->timestamps();
+        });
+
+        Schema::create('capsules_media', function (Blueprint $table){
+            $table->id();
+            $table->foreignIdFor(Capsule::class)->constrained()->onDelete('cascade');
+            $table->string('media_type');
+            $table->text('file_path');
+            $table->string('file_name');
+            $table->string('mood');
+            $table->timestamps();
+        });
+
+        Schema::create('capsules_tags', function (Blueprint $table){
+            $table->id();
+            $table->foreignIdFor(Capsule::class)->constrained()->onDelete('cascade');
+            $table->boolean('private_mode')->default(false);
+            $table->boolean('surprize_mode')->default(false);
+            $table->boolean('location_mode')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::create('capsule_locations', function (Blueprint $table){
+            $table->id();
+            $table->foreignIdFor(Capsule::class)->constrained()->onDelete('cascade');
+            $table->string('ip_address')->nullable();
+            $table->decimal('latitude', 10, 7)->nullable(); #10, 7 means 10 decimals with 7 after the point
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->timestamps();
+
+
         });
     }
 
