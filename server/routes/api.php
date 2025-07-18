089636632bf247ee84eb05dba45448741cbd7606
ALi Shhoury <alishhoury78@gmail.com>
2025-07-18 05:57:08 +0300

    hello world

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get("/", function (Request $request) {
+    return"hello world";
+}
+
+);
