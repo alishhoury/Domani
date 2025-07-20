e966e5aaef301aa4d649d7033479e85020ecde1f
ALi Shhoury <alishhoury78@gmail.com>
2025-07-20 22:22:50 +0300

    created the services

---
+++ server/app/Services/Capsule/RetrieveCapsuleService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Capsule;
+
+class RetrieveCapsuleService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
