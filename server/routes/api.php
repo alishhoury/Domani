d559d9da560444d39c3c8303b07cab5259efd67f
ALi Shhoury <alishhoury78@gmail.com>
2025-07-20 11:06:15 +0300

    created main controllers files

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,9 +3,28 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use app\Http\Controllers\User\AuthController;
 
-Route::get("/", function (Request $request) {
-    return"hello world";
-}
 
-);
+
+
+//Private Apis (need to be logged in)
+Route::post("/logout", [AuthController::class, "logout"]);
+
+Route::post("/create", [])
+
+
+
+
+
+
+
+
+
+
+//Public Apis (Every one can access)
+Route::post("/login", [AuthController::class, "login"]);
+Route::post("/register", [AuthController::class, "register"]);
+
+
+
