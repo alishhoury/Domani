6142ed911dbb59d7b7f287cbfed26b996e72b4e6
ALi Shhoury <alishhoury78@gmail.com>
2025-07-20 11:10:36 +0300

    created create capsule controller route

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use app\Http\Controllers\User\AuthController;
+use app\Http\Controllers\User\ProfileController;
+use app\Http\Controllers\Capsule\CreateCapsuleController;
+use app\Http\Controllers\Capsule\RetrieveCapsuleController;
 
 
 
@@ -11,8 +14,7 @@ use app\Http\Controllers\User\AuthController;
 //Private Apis (need to be logged in)
 Route::post("/logout", [AuthController::class, "logout"]);
 
-Route::post("/create", [])
-
+Route::post("/create", [CreateCapsuleController::class, "create"]);
 
 
 
