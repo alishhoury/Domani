641d8b83970b305902a2cc54b53ff986db48547b
ALi Shhoury <alishhoury78@gmail.com>
2025-07-21 11:04:16 +0300

    used response trait succefully

--- server/app/Http/Controllers/Capsule/RetrieveCapsuleController.php
+++ server/app/Http/Controllers/Capsule/RetrieveCapsuleController.php
@@ -20,12 +20,14 @@ class RetrieveCapsuleController extends Controller
         
         $Capsules = Capsule::with(['media', 'tag', 'location'])->where('is_revealed', 0)->whereHas('tag', function ($query) {
         $query->where('private_mode', 0); })
-        ->orderBy('reveal_at', 'desc')->take(20)->get();        
+        ->orderBy('reveal_at', 'desc')->take(20)->get();    
 
-        $response = [];
-        $response["status"] = "success";
-        $response["payload"] = $Capsules;
-        return json_encode($response);
+        return $this->responseJSON($Capsules);
+
+        // $response = [];
+        // $response["status"] = "success";
+        // $response["payload"] = $Capsules;
+        // return json_encode($response);
     }
 
     function getPublicByCountry(){
--- server/app/Traits/ResponseTrait.php
+++ server/app/Traits/ResponseTrait.php
@@ -2,13 +2,13 @@
 
 namespace App\Traits;
 
-class ResponseTrait
-{
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
+trait ResponseTrait{
+    
+    static function responseJSON($payload, $status = "success", $status_code = 200){
+        return response()->json([
+            "status" => $status,
+            "payload" => $payload
+        ], $status_code);
     }
+
 }
