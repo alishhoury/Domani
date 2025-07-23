9e6aa7c8854e78a247d2d0686a7d4b7fe321df05
ALi Shhoury <alishhoury78@gmail.com>
2025-07-24 00:02:14 +0300

    landing design

--- domani/src/App.jsx
+++ domani/src/App.jsx
@@ -3,14 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Auth from "./pages/Auth";
 import Landing from "./pages/Landing";
+import Home from "./pages/Home";
 
 function App() {
   return ( 
   <div className="App">
     <BrowserRouter>
       <Routes>
-          <Route path="/Auth" element={<Auth />} />
           <Route path="/" element={<Landing />} />
+          <Route path="/Auth" element={<Auth />} />
+          <Route path="/Home" element={<Home />} />
+
 
       </Routes>
     </BrowserRouter>    
--- domani/src/pages/Landing/index.jsx
+++ domani/src/pages/Landing/index.jsx
@@ -7,42 +7,33 @@ import Hero from "../../assets/hero.png"
 const Landing = ({}) => {
   const navigate = useNavigate();
 
-  const handleGetStarted = () => {
+  const GetStarted = () => {
     navigate('/Auth');
   };
 
   return (
     <div className="landing-page">
-      <main className="main-content">
-        <div className="hero">
-          <div className='content'>
-            <img src={Title} alt="Domani" />
-            <h1>Send your heart <br />into tomorrow</h1>
-            <p>For the things that can't be said today,
-                <br />
-                save a message in a time capsule
-                <br />
-                to be unlocked in the future. 
-            </p>
-            <button className='get-started-button' onClick={handleGetStarted}>
-              Get Started
-            </button>
-          </div>
-        </div>
-        <div className="hero-image">
-          <img className='capsule' src={Hero} alt="Domani"/>
-        </div>
-      </main>
+        <hero>
+            <div class="asside">
+                <img class="Title" src={Title} alt = "Domani"/>
+                <h1>Send your heart<br/> into tommorrow</h1>
+                <p>For the things that can’t be said today,<br/>
+                    save a message in a time capsule<br/>
+                    to be unlocked in the future. </p>
+                <button onClick={GetStarted}>Get Started</button>
+            </div>
 
-      <footer>
-        <div className="footer-content">
-          <div className="message">
-            <h4>DOMANI</h4>
-            <p>Because not all messages are meant for today</p>
-          </div>
-          <p className="copyright">&copy; 2025 Domani, All rights reserved</p>
-        </div>
-      </footer>
+            <div>
+                <img class="capsule" src={Hero} alt = "Domani"/>
+            </div>
+        </hero>
+        <footer>
+
+            <h4>DOMANI Because not all messages are meant for today</h4>
+            <p>&copy; 2025 Domani. All rights reserved.</p>
+
+
+        </footer>
     </div>
   );
 }
