f08e734efec29a817fb6989db0e0b11b27b77d4d
ALi Shhoury <alishhoury78@gmail.com>
2025-07-23 23:02:48 +0300

    desgin landing page

--- domani/src/pages/Landing/index.jsx
+++ domani/src/pages/Landing/index.jsx
@@ -4,37 +4,47 @@ import "./styles.css";
 import Title from "../../assets/title.svg"
 import Hero from "../../assets/hero.png"
 
-
-const Landing = ({}) =>{
+const Landing = ({}) => {
   const navigate = useNavigate();
 
-  const GetStarted = () => {
+  const handleGetStarted = () => {
     navigate('/Auth');
   };
 
   return (
     <div className="landing-page">
-        <div className='content'>
+      <main className="main-content">
+        <div className="hero">
+          <div className='content'>
             <img src={Title} alt="Domani" />
             <h1>Send your heart <br />into tomorrow</h1>
-            <p>For the things that can’t be said today,
+            <p>For the things that can't be said today,
                 <br />
                 save a message in a time capsule
-            <br />
-                to be unlocked in the future. </p>
-
-            <button className='get-started-button' onClick={GetStarted}>Get Started</button>
+                <br />
+                to be unlocked in the future. 
+            </p>
+            <button className='get-started-button' onClick={handleGetStarted}>
+              Get Started
+            </button>
+          </div>
         </div>
-        <img src={Hero} alt="Domani"/>
-        <footer>
-            <div>
-                <p>DOMANI</p>
-                <p>Because not all messages are meant for today</p>
-            </div>
-            <p>&copy; 2025 Domani, All rights reserved</p>
-        </footer>
+        <div className="hero-image">
+          <img className='capsule' src={Hero} alt="Domani"/>
+        </div>
+      </main>
 
+      <footer>
+        <div className="footer-content">
+          <div className="message">
+            <h4>DOMANI</h4>
+            <p>Because not all messages are meant for today</p>
+          </div>
+          <p className="copyright">&copy; 2025 Domani, All rights reserved</p>
+        </div>
+      </footer>
     </div>
   );
 }
+
 export default Landing;
