64e8cab4d5ff2469325f1b7f265a5e92027b9209
ALi Shhoury <alishhoury78@gmail.com>
2025-07-24 07:42:40 +0300

    added routes with middleware

--- server/routes/api.php
+++ server/routes/api.php
@@ -10,31 +10,29 @@ use App\Http\Controllers\Capsule\CreateCapsuleController;
 use App\Http\Controllers\Capsule\RetrieveCapsuleController;
 
 
-//Private Apis (need to be logged in)
+Route::group(["middleware" => "auth:api"], function(){
+
 Route::post("/logout", [AuthController::class, "logout"]);
 
-//create
 Route::post("/create", [CreateCapsuleController::class, "create"]);
 
-//Retrieve
 Route::get("/UserCapsules", [RetrieveCapsuleController::class, "getUserCapsules"]);
 Route::get("/PrivateCapsules", [RetrieveCapsuleController::class, "getPrivateCapsules"]);
 Route::get("/PublicCapsules", [RetrieveCapsuleController::class, "getPublicCapsules"]);
 Route::get("/FilterByCountry", [RetrieveCapsuleController::class, "getPublicByCountry"]);
 Route::get("/FilterByMood", [RetrieveCapsuleController::class, "getPublicByMood"]);
 
-//update
 Route::post("/UpdatePhoto", [ProfileController::class, "updatePhoto"]);
 
+});
 
 
+Route::group(["prefix" => "guest"], function(){
 
-
-//Public Apis (Every one can access)
 Route::post("/login", [AuthController::class, "login"]);
 Route::post("/register", [AuthController::class, "register"]);
 
-//retrieve by link
 Route::get("/CasuleByLink", [RetrieveCapsuleController::class, "getUnlisted"]);
+});
 
 
