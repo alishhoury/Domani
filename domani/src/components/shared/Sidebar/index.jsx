de1b191566b5c1ccdf989c1f7cda910534595cb8
ALi Shhoury <alishhoury78@gmail.com>
2025-07-24 06:46:37 +0300

    added logout button

--- domani/src/components/shared/Sidebar/index.jsx
+++ domani/src/components/shared/Sidebar/index.jsx
@@ -1,23 +1,49 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import "./styles.css";
 import title from "../../../assets/title.svg";
-import home from "../../../assets/home.svg";
-import create from "../../../assets/create.svg";
-import private1 from "../../../assets/private.svg";
-import profile from "../../../assets/profile.svg";
+import Home from "../../../assets/home.svg";
+import Create from "../../../assets/create.svg";
+import Private1 from "../../../assets/private.svg";
+import Profile from "../../../assets/profile.svg";
 import line from "../../../assets/Line.svg";
+import Logout from "../../../assets/logout.svg";
 
 const SideBar = ({}) => {
+    const navigate = useNavigate();
+
+      const home = () => {
+        navigate('/home');
+  };
+
+      const create = () => {
+        navigate('/create');
+  };
+
+      const private1 = () => {
+        navigate('/private');
+  };
+
+      const profile = () => {
+         navigate('/profile');
+  };
+
+      const logout = () => {
+         navigate('/');
+  };
+
     return (
         <div className="SideBar">
 
             <div class="bar-content">
                 <img src={title} alt="Domani" />
 
-                <img src={home} alt="home" />
-                <img src={create} alt="create" />
-                <img src={private1} alt="private" />
-                <img src={profile} alt="profile" />
+                <img src={Home} alt="home" onClick={home}/>
+                <img src={Create} alt="create" onClick={create}/>
+                <img src={Private1} alt="private" onClick={private1}/>
+                <img src={Profile} alt="profile" onClick={profile}/>
+                <img src={Logout} alt="logout" onClick={logout}/>
+
 
             </div>
                 <img class="line" src={line} alt="line" />
